f22bb698a3b7435b18ab35cc5dbd78ed5b1a3678
ikedaema2018 <ikedaema0817@gmail.com>
2019-01-12 20:07:58 +0900

    認証成功

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,19 +1,16 @@
 class AdminUsersController < ApplicationController
   def create
-    @user = User.find_by(email: params[:email])
-    if @user
-      render json: @user
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      render json: user
     else
       render status: 403, json: { aaa: "aaaa" }
     end
+
+  rescue
+    render status: 500, json: {bbb: "bbbb"}
   end
 
-  def index
-    @jjj = [
-      aaa: 222,
-      bbb: 333
-    ]
-    render @jjj
-  end
+
   
 end
