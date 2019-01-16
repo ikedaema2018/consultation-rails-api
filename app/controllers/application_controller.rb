030c6da5af22ce91d9da1133a98c245fc753c45c
ikedaema2018 <ikedaema0817@gmail.com>
2019-01-16 12:00:58 +0900

    Userモデルのテスト作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::API
   # protect_from_forgery with: :exception
   # rescue_from Exception, with: :handle500
 
+  config.generators do |g|
+    g.test_framework :rspec,
+    
+    fixtures: false,
+    view_specs: false,
+    heloper_specs: false,
+    routing_specs: false
 
-  # def handle500
-  #   render status: 500
-  # end
+  end
+
+
 end
